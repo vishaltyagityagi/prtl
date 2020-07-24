de5d501251643d00d907d453a0b3d4cb93fdded2
Shubham Sharma <shubham.sharma@squaredsd.com>
2020-07-24 20:00:50 +0530

    added pages basic themes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,27 @@
+class PagesController < ApplicationController
+
+  def services
+  end
+
+  def single_services
+  end
+
+  def blog_single
+  end
+
+  def protfolio
+  end
+
+  def protfolio_single
+  end
+
+  def testmonials
+  end
+
+  def ask_questions
+  end
+
+  def gallery
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,19 @@ Rails.application.routes.draw do
   get 'single_job' => 'dashboard#single_job'
   get 'post_job' => 'dashboard#post_job'
   get 'blog' => 'dashboard#blog'
+
+   resources :pages do
+    collection do
+      get :services
+      get :single_services
+      get :blog_single
+      get :protfolio
+      get :protfolio_single
+      get :testmonials
+      get :ask_questions
+      get :gallery
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
